86904c30130c6e2f4594a24569356bb7a662a397
Renan Almeida <renanalmeidads@gmail.com>
2019-04-26 01:52:27 -0300

    Reestruturação parcial continuação

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 
 const port = process.env.PORT || 4000;
+
+const users = require("./routes/api/users");
+
+const app = express();
 
 //Configuraçao do banco de dados
 const dbConnString = require("./config/keys").mongoURI;
@@ -14,5 +17,8 @@ mongoose
     console.log("MongoDB database connection established successfully")
   )
   .catch(err => console.log(err));
+
+//Rotas
+app.use("/api/users", users);
 
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
